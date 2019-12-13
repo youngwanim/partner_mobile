8711ac5465a8f269c1c0dca425442bb26363ce02
youngwanim <ywlim98@gmail.com>
2019-12-13 10:59:49 +0900

    [youngwanim]init partner mobile page;

---
+++ src/components/RandomChart.vue
@@ -0,0 +1,58 @@
+<template>
+  <div >
+  <v-card class="pa-4 ma-4">
+    <LineChart :chartData="datacollection"></LineChart>
+    <v-card-actions>
+      <v-btn @click="fillData()">Randomize</v-btn>
+    </v-card-actions>
+  </v-card>
+  </div>
+</template>
+
+<script>
+  import LineChart from './charts/LineChart.js'
+
+  export default {
+    components: {
+      LineChart
+    },
+    data () {
+      return {
+        datacollection: null
+      }
+    },
+    mounted () {
+      console.log('mounted')
+      this.fillData()
+    },
+    methods: {
+      fillData () {
+        this.datacollection = {
+          labels: [this.getRandomInt(), this.getRandomInt()],
+          datasets: [
+            {
+              label: 'Data One',
+              backgroundColor: '#f87979',
+              data: [this.getRandomInt(), this.getRandomInt()]
+            }, {
+              label: 'Data One',
+              backgroundColor: '#f87979',
+              data: [this.getRandomInt(), this.getRandomInt()]
+            }
+          ]
+        }
+        console.log(JSON.stringify(this.datacollection))
+      },
+      getRandomInt () {
+        return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+      }
+    }
+  }
+</script>
+
+<style>
+  .small {
+    max-width: 600px;
+    margin:  150px auto;
+  }
+</style>
